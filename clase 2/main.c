f41576320747b1b8971030ae450aa8ca31179bf5
Facundo Dominguez <facundoariel.dominguez@gmail.com>
2018-03-22 20:57:55 -0300

    Add files via upload
    clase 2

---
+++ clase 2/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define CANTIDAD 5
+
+
+int suma;
+int num;
+int i;
+float promedio;
+int mayor;
+int menor;
+int promedio2;
+int contador;
+
+suma=0;
+
+
+
+
+int main()
+
+{
+    printf("INGRESAR NUMERO:");
+    scanf("%d",&num);
+
+    for(contador=1; contador< CANTIDAD+1; contador++)
+    {
+        if()
+            printf("ingresar numero:");
+        scanf("%d",&num);
